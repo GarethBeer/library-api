ca96e9b3676742cd1fc4d4ada87ddd68e0df05db
GarethBeer <gareth.beer1989@outlook.com>
2020-02-13 16:45:20 +0000

    refactoring tests using a helper and data factory so tests are cleaner

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/User');
+const userHelpers = require('./helpers/user-helpers');
+const DataFactory = require('./helpers/data-factory');
 
 describe('Users', () => {
   beforeAll(done => {
@@ -26,42 +27,31 @@ describe('Users', () => {
 
   describe('POST/ user', () => {
     it('posts users details to the database', done => {
-      request(app)
-        .post('/users')
-        .send({
-          firstName: 'Doug',
-          lastName: 'Douglas',
-          email: 'DD@hotmail.org',
-          password: 'hello12345',
-        })
+      const data = DataFactory.user();
+      userHelpers
+        .signUp(app, data)
         .then(res => {
           expect(res.status).toBe(201);
 
           User.findById(res.body._id, (err, user) => {
-            expect(user.firstName).toBe('Doug');
-            expect(user.lastName).toBe('Douglas');
-            expect(user.email).toBe('DD@hotmail.org');
+            expect(user.firstName).toBe(data.firstName);
+            expect(user.lastName).toBe(data.lastName);
+            expect(user.email).toBe(data.email);
             expect(user.password).not.toBe('hello12345');
             expect(user.password.length).toBe(60);
             expect(res.body).not.toHaveProperty('password');
             done();
           });
-        });
+        })
+        .catch(error => done(error));
     });
 
     it('it errors if not a vaild email is entered', done => {
-      request(app)
-        .post('/users')
-        .send({
-          firstName: 'Gareth',
-          lastName: 'Beer',
-          email: 'thisisntavalidemail@',
-          password: 'blahblah123',
-        })
-        .then(res => {
-          expect(res.status).toBe(400);
-          expect(res.body.errors.email).toBe('Invalid email address');
-        });
+      const data = DataFactory.user({ email: '111111111@' });
+      userHelpers.signUp(app, data).then(res => {
+        expect(res.status).toBe(400);
+        expect(res.body.errors.email).toBe('Invalid email address');
+      });
 
       User.countDocuments({}, function(err, count) {
         console.log(count);
@@ -71,19 +61,12 @@ describe('Users', () => {
     });
 
     it('errors if password is not 8 or more characters long', done => {
-      request(app)
-        .post('/users')
-        .send({
-          firstName: 'Gareth',
-          lastName: 'Beer',
-          email: 'hellow@hotmail.com',
-          password: 'blah',
-        })
-        .then(res => {
-          expect(res.status).toBe(400);
-          expect(res.body.errors.password).toBe('Password must be at least 8 characters long');
-          done();
-        });
+      const data = DataFactory.user({ password: 'pass' });
+      userHelpers.signUp(app, data).then(res => {
+        expect(res.status).toBe(400);
+        expect(res.body.errors.password).toBe('Password must be at least 8 characters long');
+        done();
+      });
     });
   });
 });
